920d5d9affd83c1bd996978f798636e1a4c865df
“Nader <nadmot23@gmail.com>
2024-09-30 13:31:44 -0700

    Deploy commit

--- dist/index.js
+++ dist/index.js
@@ -76,7 +76,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ About)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"./node_modules/react/jsx-runtime.js\");\n\nfunction About() {\n  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"div\", {\n    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"h1\", {\n      className: \"text-red-600 text-2xl font-bold\",\n      children: \" ABOUT PAGE! \"\n    })\n  });\n}\n\n//# sourceURL=webpack://sweetcutecreamery/./src/routes/About/About.jsx?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ About)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"./node_modules/react/jsx-runtime.js\");\n\nfunction About() {\n  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"div\", {\n    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"h1\", {\n      className: \"text-red-600 text-2xl font-bold\",\n      children: \" ABOUT PAGE\"\n    })\n  });\n}\n\n//# sourceURL=webpack://sweetcutecreamery/./src/routes/About/About.jsx?");
 
 /***/ }),
 
--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h1 className="text-red-600 text-2xl font-bold"> ABOUT PAGE! </h1>
+      <h1 className="text-red-600 text-2xl font-bold"> ABOUT PAGE</h1>
     </div>
   );
 }
