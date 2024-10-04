f63eac3f3551005c486f6a9fa53ae497619360fe
“Nader <nadmot23@gmail.com>
2024-10-04 11:55:43 -0700

    Updated selection and styling and flavors

--- sweetcute/src/Pages/Flavors.tsx
+++ sweetcute/src/Pages/Flavors.tsx
@@ -1,5 +1,3 @@
-import { faIceCream } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { flavors } from "../data";
 
 export function Flavors() {
@@ -7,40 +5,27 @@ export function Flavors() {
   return (
     <>
       <div>
-        <h2 className="m-auto text-center">
+        <h2 className="m-auto text-center py-6">
           CURRENT FLAVORS (but we’re always dreaming up more!)
         </h2>
       </div>
-      <div className="flex flex-wrap items-baseline">
-        {flavors.map((flavor) => {
-          return (
-            <FlavorWrapper
-              title={flavor.title}
-              description={flavor.description}
-              key={keyCount++}
-            />
-          );
-        })}
+      <div className="flex justify-around text-center pb-44">
+        <ul>
+          <h2 className="font-bold">OUR FOREVER FLAVORS</h2>
+          {flavors.foreverFlavors.map((flavor) => {
+            return <li key={keyCount++}>{flavor.title}</li>;
+          })}
+        </ul>
+        <ul>
+          <h2 className="font-bold">OUR SIDE PIECES</h2>
+          <h2 className="font-semibold">
+            (AVAILABLE ON ROTATION FOR LIMITED TIMES)
+          </h2>
+          {flavors.sidePieces.map((flavor) => {
+            return <li key={keyCount++}>{flavor.title}</li>;
+          })}
+        </ul>
       </div>
     </>
-  );
-}
-
-function FlavorWrapper({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) {
-  return (
-    <div className="flex flex-col justify-center items-center basis-1/3 my-8 px-1">
-      <h2>{title}</h2>
-      <FontAwesomeIcon
-        icon={faIceCream}
-        style={{ height: "2rem", margin: "1rem" }}
-      />
-      <h3>{description}</h3>
-    </div>
   );
 }
