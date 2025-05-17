ef41815e55223e9212b255d447a3507684f9963d
“Nader <nadmot23@gmail.com>
2025-05-17 11:09:37 -0700

    Updated styling

--- sweetcute/src/Pages/Catering.tsx
+++ sweetcute/src/Pages/Catering.tsx
@@ -21,7 +21,10 @@ export function Catering() {
       <div className="flex flex-col items-center relative flex-wrap bg-[#FAE498] w-full min-h-svh">
         <div className="text-center py-1 md:pb-4 pb-20 w-full">
           <h2 className="font-FaroVariable text-[#FF0000] text-xl md:text-4xl my-10 mb-4 underline">
-            Artisan Ice Cream Catering
+            Artisanal Ice Cream Catering
+          </h2>
+          <h2 className="font-FaroVariable text-black text-xl md:text-4xl my-10 mb-4 underline">
+            Package Tiers
           </h2>
           <div className="flex flex-wrap justify-between mx-3 md:mx-16">
             {cateringTiers.map((tier, index) => (
@@ -29,15 +32,17 @@ export function Catering() {
             ))}
           </div>
           <div className="flex flex-wrap justify-center mx-3 md:mx-16">
-            <div className="basis-[50%] h-72 my-2 md:my-4 rounded-2xl">
-              <div className={`relative w-full h-full rounded-2xl`}>
-                <div className="absolute w-full rounded-2xl h-full backface-hidden flex items-center justify-center bg-[#FFF5C1]">
+            <div className="basis-[40%] h-64 my-2 md:my-4 rounded-2xl">
+              <div className="relative w-full h-full rounded-2xl">
+                <div className="absolute w-full rounded-2xl h-full backface-hidden flex items-center justify-center bg-[#94cbf8]">
                   <div className=" h-full w-full rounded-2xl flex flex-col justify-center items-center">
-                    <div className="basis-[25%] w-full h-8 text-center">
-                      All Packages Include
+                    <div className="w-full text-center flex justify-center mt-5">
+                      <h3 className="text-black font-FaroVariable text-base md:text-xl xl:text-3xl p-2  bg-white rounded-xl w-fit h-fit">
+                        All Packages Include
+                      </h3>
                     </div>
-                    <div className="basis-[75%] relative text-center">
-                      <ul>
+                    <div className="relative ">
+                      <ul className="text-black font-PoppinsLight text-xs md:text-lg xl:text-xl p-2 pt-3 lg:p-6 text-left list-disc">
                         <li>Hand Scooped Premium Ice Cream</li>
                         <li>1 Scooper</li>
                         <li>2 Hours of service (plus 30 minute set up)</li>
@@ -52,7 +57,7 @@ export function Catering() {
           </div>
         </div>
         <div className="text-center py-1 md:pb-4 pb-20 w-full flex flex-col items-center">
-          <h2 className="font-FaroVariable text-[#FF0000] text-xl md:text-4xl my-10 mb-4 underline">
+          <h2 className="font-FaroVariable text-black text-xl md:text-4xl my-10 mb-4 underline">
             Flavors:
           </h2>
           <div className="flex flex-wrap justify-between content-center mx-3 md:mx-16 w-[70%]">
@@ -92,19 +97,21 @@ export function Catering() {
 
 function PackageCard({ tier }: { tier: CateringTier }) {
   return (
-    <div className="basis-[20%] h-72 my-2 md:my-4 rounded-2xl">
-      <div className={`relative w-full h-full rounded-2xl`}>
-        <div className="absolute w-full rounded-2xl h-full backface-hidden flex items-center justify-center bg-[#FFF5C1]">
+    <div className="basis-[23%] h-60 my-2 md:my-4 rounded-2xl">
+      <div className="relative w-full h-full rounded-2xl">
+        <div className="absolute w-full rounded-2xl h-full backface-hidden flex items-center justify-center bg-[#94cbf8]">
           <div className=" h-full w-full rounded-2xl flex flex-col justify-center items-center">
-            <div className="basis-[25%] w-full h-8 text-center">
-              {tier.guestRange} GUESTS
+            <div className="basis-[25%] w-full text-center flex justify-center mt-5">
+              <h3 className="text-black font-FaroVariable text-base md:text-xl xl:text-3xl p-2 bg-white rounded-xl w-fit h-fit">
+                {tier.guestRange} GUESTS
+              </h3>
             </div>
-            <div className="basis-[40%] relative text-center">
-              <h2 className="text-[#FF0000] font-FaroVariable text-base md:text-3xl px-2 md:px-6 overflow-visible leading-none relative -top-2">
+            <div className="basis-[40%] relative text-center mt-5">
+              <h2 className="text-[#FF0000] font-FaroVariable text-base md:text-3xl px-2 md:px-6 overflow-visible leading-none relative">
                 ${tier.price}
               </h2>
             </div>
-            <div className="basis-[35%] lg:h-36 h-24 text-center relative">
+            <div className="basis-[35%] text-center relative text-black font-PoppinsLight text-xs md:text-lg xl:text-xl p-2 pt-3 lg:p-6">
               <h3>{tier.flavorCount} Flavors</h3>
               <h3>${tier.addFlavorPrice} for additional flavor</h3>
             </div>
